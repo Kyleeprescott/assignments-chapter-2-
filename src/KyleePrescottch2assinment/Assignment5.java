1202e02d81c2451e164c04df3269f286535db2e0
kylee.prescott <kylee.prescott@CLC-LAB209-09.TCSD.tooelesd.org>
2018-09-10 14:17:17 -0600

    assigments 2

---
+++ src/KyleePrescottch2assinment/Assignment5.java
@@ -0,0 +1,15 @@
+package KyleePrescottch2assinment;
+
+public class Assignment5 {
+
+	public static void main(String[] args) {
+      System.out.println("welcome to java ");
+      System.out.println("welcome to java ");
+      System.out.println("welcome to java ");
+      System.out.println("welcome to java ");
+      System.out.println("welcome to java ");
+    
+
+	}
+
+}
---
+++ src/KyleePrescottch2assinment/Ch2Assinment.java
@@ -0,0 +1,16 @@
+package KyleePrescottch2assinment;
+/*Kylee Prescott
+ 9/18
+ chapter 2 assignments  
+ */
+public class Ch2Assinment {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programmimg is Fun");
+        
+   
+	}
+
+}
