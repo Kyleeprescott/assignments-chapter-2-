07a786f64466e47855633c7b2f7db08d6e5528eb
kylee.prescott <kylee.prescott@CLC-LAB209-09.TCSD.tooelesd.org>
2018-09-18 14:05:03 -0600

    assingmet 2

---
+++ src/KyleePrescottch2assinment/FavoriteQuote.java
@@ -0,0 +1,14 @@
+package KyleePrescottch2assinment;
+/*Kylee Prescott
+ * 9/18
+ * Print out your favorite quote
+ */
+
+public class FavoriteQuote {
+
+	public static void main(String[] args) {
+System.out.println(" \"Keep your eyes on the stars and your feet ont the ground\" Teddy Roosevelt");
+
+	}
+
+}
---
+++ src/KyleePrescottch2assinment/PrintedTable.java
@@ -0,0 +1,18 @@
+package KyleePrescottch2assinment;
+/*Kylee prescott
+ * 9/18
+ * Create a table 
+ */
+public class PrintedTable {
+
+	public static void main(String[] args) {
+		// print a single line 
+		System.out.println("a\ta^2\ta^3");
+		System.out.println("1\t1\t1");
+		System.out.println("2\t4\t8");
+		System.out.println("2\t9\t27");
+		System.out.println("4\t16\t64");
+
+	}
+
+}
