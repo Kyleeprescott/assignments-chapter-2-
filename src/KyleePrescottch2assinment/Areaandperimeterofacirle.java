db87aca0dc370ba53fa4769cda30b35bbb673ea9
kylee.prescott <kylee.prescott@CLC-LAB209-09.TCSD.tooelesd.org>
2018-09-20 14:05:51 -0600

    assignmets for chapter 2

---
+++ src/KyleePrescottch2assinment/Areaandperimeterofacirle.java
@@ -0,0 +1,33 @@
+package KyleePrescottch2assinment;
+import java.util.Scanner;
+/*kylee prescott
+ * 9/18
+ * 
+ */
+
+public class Areaandperimeterofacirle {
+
+	public static void main(String[] args) {
+Scanner input=new Scanner(System.in);
+		
+		//step one:enter the radius of a circle
+		 double area;
+		 double radius=5.5;
+		 double perimeter= 2;
+		
+		 
+		//constant declinations 
+		 final  double PI= 3.1415926535979323846264338327950288419716939;
+		  area = 5.5*5.5*PI;
+		  perimeter =  2*5.5*PI;
+		  
+		 
+		
+		  System.out.println("The area of your circle is:"+ area +"\"");
+		  System.out.println("the perimeter of you circle is"+perimeter+"/");
+		  
+		  	
+
+	}
+
+}
---
+++ src/KyleePrescottch2assinment/Areaofrectangle.java
@@ -0,0 +1,35 @@
+package KyleePrescottch2assinment;
+import java.util.Scanner;
+
+/*kylee prescott
+ * 
+ * 
+ */
+public class Areaofrectangle {
+
+	public static void main(String[] args) {
+		
+			Scanner input=new Scanner(System.in);
+					
+					//step one:enter the radius of a circle
+					 double width = 4.5;
+					 double height=7.9;
+					 double area;
+					 double perimeter;
+					 
+					 area = 4.5*7.9;
+					 perimeter= 4.5+4.5+7.9+7.9;
+					 
+					 
+					 
+
+					  System.out.println("The area of your rectangle is:"+ area +"\"");
+					  System.out.println("the perimeter of you rectangle is"+perimeter+"\"");
+					  
+					  	
+					 
+					
+
+	}
+
+}
